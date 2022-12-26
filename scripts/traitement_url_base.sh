1a37e11379d3ea9512c7a336753361a4f73e1d1e
cbuontal <aubanel.buon@gmail.com>
2022-12-26 10:45:39 +0100

    update tableau zh

--- scripts/traitement_url_base.sh
+++ scripts/traitement_url_base.sh
@@ -55,7 +55,7 @@ echo 	"<html>
 	<body>
 		<h1 class=\"title\">Tableau des URLs $basename</h1>
 		<table class=\"table is-bordered\">
-			<thead><tr><th>ligne</th><th>code HTTP</th><th>URL</th><th>encodage</th><th>dump</th><th>html</th><th>occurrences</th><th>contextes</th><th>concordances</th></tr></thead>" > "tableaux/$fichier_tableau"
+			<thead><tr><th>ligne</th><th>code HTTP</th><th>URL</th><th>encodage</th><th>html</th><th>dump</th><th>occurrences</th><th>contextes</th><th>concordances</th></tr></thead>" > "tableaux/$fichier_tableau"
 
 
 # pour chaque URL du fichier URL :
@@ -70,7 +70,7 @@ do
 	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1)
 	# récupération de l'encodage
 	charset=$(curl -ILs $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
-	
+
 	if [[ -z $charset ]]
 	then
 		echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
@@ -80,52 +80,45 @@ do
 	fi
 	# pour transformer les 'utf-8' en 'UTF-8' :
 	charset=$(echo $charset | tr "[a-z]" "[A-Z]")
-	
+
 	if [[ $code -eq 200 ]]
 	then
-		dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL)
-		if [[ $charset -ne "UTF-8" && -n "$dump" ]]
+		aspiration=$(curl $URL)
+
+		if [[ $charset == 'UTF-8' ]]
 		then
-			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE)
+			dump=$(curl $URL | lynx -stdin -dump -assume_charset=utf-8 -display_charset=utf-8)
+		else
+			# charset=$(curl $URL | urchardet)
+			dump=$(curl $URL | iconv -f $charset -t UTF-8 | lynx -stdin -dump -assume_charset=utf-8 -display_charset=utf-8)
 		fi
 	else
 		echo -e "\tcode différent de 200 utilisation d'un dump vide"
 		dump=""
 		charset=""
 	fi
-	
+
 	echo "$dump" > "./dumps-text/$basename-$lineno.txt"
-	
-	aspiration=$(curl $URL)
+
 	echo "$aspiration" > "./aspirations/$basename-$lineno.html"
 
 	# compte du nombre d'occurrences
-  	NB_OCC=$(grep -E -o $mot ./dumps-text/$basename-$lineno.txt | wc -l)
+  NB_OCC=$(grep -E -o $mot ./dumps-text/$basename-$lineno.txt | wc -l)
 
-  	# extraction des contextes
-  	grep -E -A2 -B2 $mot ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
+  # extraction des contextes
+  grep -E -A2 -B2 $mot ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
 
-  	# construction des concordances avec une commande externe
-  	bash scripts/concordance.sh ./dumps-text/$basename-$lineno.txt $mot > ./concordances/$basename-$lineno.html
+  # construction des concordances avec une commande externe
+  bash scripts/concordance.sh ./dumps-text/$basename-$lineno.txt $mot > ./concordances/$basename-$lineno.html
 
-	
-	
+
+
 	echo "			<tr><td>$lineno</td><td>$code</td><td>$URL</td><td>$charset</td><td><a href="../aspirations/$basename-$lineno.html">html</a></td><td><a href="../dumps-text/$basename-$lineno.txt">text</a></td><td>$NB_OCC</td><td><a href="../contextes/$basename-$lineno.txt">contextes</a></td><td><a href="../concordances/$basename-$lineno.html">concordance</a></td></tr>" >> "tableaux/$fichier_tableau"
 	lineno=$((lineno+1));
 done < $fichier_urls
 
-#URL="https://new.qq.com/rain/a/20221025A05OLS00"
-#CODE_HTTP=$(curl -I $URL | grep HTTP | cut -d ' ' -f 2)
-
-#echo "<tr><td>1</td><td>$CODE_HTTP</td><td>$URL</td></tr>\n" >> tableau.html
 
 #on ferme le fichier
 echo "		</table>
-	</body> 
+	</body>
 </html>" >> "tableaux/$fichier_tableau"
-
-
-
-# modifier la ligne suivante pour créer effectivement du HTML
-#echo "<html>\n<head>\n  <meta charset=\"utf-8\" />\n</header>\n<body>\n  <table>" > $fichier_tableau
-#echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
