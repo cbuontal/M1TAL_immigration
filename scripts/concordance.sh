29f8917e0520544acbecfba6df576e53a0aba550
MichaBiriuchinskii <yosuga723@mail.ru>
2023-01-06 18:43:18 +0100

    ajout des fichiers russes

--- scripts/concordance.sh
+++ scripts/concordance.sh
@@ -35,34 +35,37 @@ then
 	exit
 fi
 
-echo "
-<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-  <meta charset=\"UTF-8\">
-  <title>Concordance</title>
-</head>
-<body>
-<table>
-<thead>
-  <tr>
-    <th class=\"has-text-right\">Contexte droit</th>
-    <th>Cible</th>
-    <th class=\"has-text-left\">Contexte gauche</th>
-  </tr>
-</thead>
-<tbody>
-"
+echo 	"
+            <html>
+             <html lang=\"ru\">
+			 <head>
+							<meta charset=\"utf-8\" /> 
+							<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
+							<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+							<title>Concordance</title>
+			</head>
+			<body>
+							<h1 class=\"title\">Concordance</h1>
+							<table class=\"table is-bordered is-striped is-narrow is-hoverable is-fullwidth\">
+									<thead>
+									<tr>
+									<th class=\"has-text-right\">Contexte gauche</th>
+									<th>Cible</th>
+									<th class=\"has-text-left\">Contexte droit</th>
+									</tr>
+									</thead>
+									" 
 
 if [[ $lang == 'fr' ]]
 then
 	echo "fr"
 elif [[ $lang == 'ru' ]]
 then
-	echo "ru"
+	#pour utiliser cette ligne, il faut installer gnu-sed : brew install gnu-sed
+	grep -E -o "(\w+\W+){0,5}\b$motif\b(\W+\w+){0,5}" $fichier_text | gsed -E "s/(.*)$motif(.*)/<tr><td class="has-text-right">\1<\/td><td class="has-text-danger">\2<\/td><td class="has-text-left">\3<\/td><\/tr>/"
 elif [[ $lang == 'zh' ]]
 then
-	grep -Po "(\p{Han}){0,5}$motif(\p{Han}){0,5}" $fichier_text | sed -E "s/(.*)($motif)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/"
+	grep -Po "(\p{Han}){0,5}$motif(\p{Han}){0,5}" $fichier_text | sed -E "s/(.*)($motif)(.*)/<tr><td class="has-text-right">\1<\/td><td class="has-text-danger">\2<\/td><td class="has-text-left">\3<\/td><\/tr>/"
 fi
 
 echo "
