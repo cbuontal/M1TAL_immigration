1a9dd458050f5ec247cdd09c877969d845d06bef
Srharahaad <sunshineflash.mlp@gmail.com>
2023-01-10 13:54:37 +0100

    Modification des scripts concordance et traitement pour inclure la navbar. Concordance ne semble plus fonctionner, même en lançant l'ancienne version, à investiguer. Ajout de la page html scripts à modifier avec du contenu à l'avenir

--- scripts/concordance.sh
+++ scripts/concordance.sh
@@ -44,7 +44,8 @@ echo 	"
 							<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 							<title>Concordance</title>
 			</head>
-			<body>
+			<body class=\"has-navbar-fixed-top\">
+							<nav class=\"navbar is-light is-fixed-top\"><div class=\"navbar-menu\"><div class=\"navbar-start\"><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#introduction\">Introduction</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#analyse\">Analyse</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../scripts.html\">Scripts</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\">Tableaux</a><div class=\"navbar-dropdown\"><a class=\"navbar-item\" href=\"../tableaux/tableau_fr.html\">Français</a><a class=\"navbar-item\" href=\"../tableaux/tableau_ru.html\">Russe</a><a class=\"navbar-item\" href=\"../tableaux/tableau_zh.html\">Chinois</a></div></div></div><div class=\"navbar-end\"><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#aPropos\">À propos</a></div><div class=\"navbar-item\"><a href=\"https://github.com/cbuontal/M1TAL_immigration\"><img src=\"../images/github_logo.png\" alt=\"Github\"></a></div></div></div></nav>
 							<h1 class=\"title\">Concordance</h1>
 							<table class=\"table is-bordered is-striped is-narrow is-hoverable is-fullwidth\">
 									<thead>
@@ -58,14 +59,14 @@ echo 	"
 
 if [[ $lang == 'fr' ]]
 then
-	grep -E -o -i "(\w+\W+| {0,5})$motif\b(\W+\w+| ){0,5}" $fichier_text | gsed -E -r "s/(.*)$motif(.*)/<tr><td class="has-text-right">\1<\/td><td class="has-text-danger">\2<\/td><td class="has-text-left">\3<\/td><\/tr>/I"
+	grep -E -o -i "(\w+\W+| {0,5})$motif\b(\W+\w+| ){0,5}" $fichier_text | gsed -E -r "s/(.*)$motif(.*)/<tr><td class=\"has-text-right\">\1<\/td><td class=\"has-text-danger\">\2<\/td><td class=\"has-text-left\">\3<\/td><\/tr>/I"
 elif [[ $lang == 'ru' ]]
 then
 	#pour utiliser cette ligne, il faut installer gnu-sed : brew install gnu-sed
-	grep -E -o -i "(\w+\W+){0,5}\b$motif\b(\W+\w+){0,5}" $fichier_text | gsed -E "s/(.*)$motif(.*)/<tr><td class="has-text-right">\1<\/td><td class="has-text-danger">\2<\/td><td class="has-text-left">\3<\/td><\/tr>/I"
+	grep -E -o -i "(\w+\W+){0,5}\b$motif\b(\W+\w+){0,5}" $fichier_text | gsed -E "s/(.*)$motif(.*)/<tr><td class=\"has-text-right\">\1<\/td><td class=\"has-text-danger\">\2<\/td><td class=\"has-text-left\">\3<\/td><\/tr>/I"
 elif [[ $lang == 'zh' ]]
 then
-	LANG=zh_CN.UTF-8 grep -Po "((\p{Han}|，){1,5} ){0,5}$motif( (\p{Han}|，){1,5}){0,5}" $fichier_text | LANG=C sed -E "s/(.*)($motif)(.*)/<tr><td class="has-text-right">\1<\/td><td class="has-text-danger">\2<\/td><td class="has-text-left">\3<\/td><\/tr>/"
+	LANG=zh_CN.UTF-8 grep -Po "((\p{Han}|，){1,5} ){0,5}$motif( (\p{Han}|，){1,5}){0,5}" $fichier_text | LANG=C sed -E "s/(.*)($motif)(.*)/<tr><td class=\"has-text-right\">\1<\/td><td class=\"has-text-danger\">\2<\/td><td class=\"has-text-left\">\3<\/td><\/tr>/"
 fi
 
 echo "
--- scripts/traitement_url_base.sh
+++ scripts/traitement_url_base.sh
@@ -63,7 +63,8 @@ echo 	"<html>
 		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 		<title>Tableau des URLS</title>
 	</head>
-	<body>
+	<body class=\"has-navbar-fixed-top\">
+		<nav class=\"navbar is-light is-fixed-top\"><div class=\"navbar-menu\"><div class=\"navbar-start\"><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#introduction\">Introduction</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#analyse\">Analyse</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../scripts.html\">Scripts</a></div><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\">Tableaux</a><div class=\"navbar-dropdown\"><a class=\"navbar-item\" href=\"tableau_fr.html\">Français</a><a class=\"navbar-item\" href=\"tableau_ru.html\">Russe</a><a class=\"navbar-item\" href=\"tableau_zh.html\">Chinois</a></div></div></div><div class=\"navbar-end\"><div class=\"navbar-item has-dropdown is-hoverable\"><a class=\"navbar-item\" href=\"../index.html#aPropos\">À propos</a></div><div class=\"navbar-item\"><a href=\"https://github.com/cbuontal/M1TAL_immigration\"><img src=\"../images/github_logo.png\" alt=\"Github\"></a></div></div></div></nav>
 		<h1 class=\"title\">Tableau des URLs $basename</h1>
 		<table class=\"table is-bordered\">
 			<thead><tr><th>ligne</th><th>code HTTP</th><th>URL</th><th>encodage</th><th>html</th><th>dump</th><th>occurrences</th><th>contextes</th><th>concordances</th></tr></thead>" > "tableaux/$fichier_tableau"
