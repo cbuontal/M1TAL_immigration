3a6cd6f5c6650ab675a224f44889a06b1f12f9ba
cbuontal <aubanel.buon@gmail.com>
2023-01-10 01:18:02 +0100

    script nuages de mots RU + nuage

--- scripts/nuage_russe.py
+++ scripts/nuage_russe.py
@@ -6,8 +6,10 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from wordcloud import WordCloud
 
+## À EXÉCUTER DEPUIS LA RACINE DU DOSSIER
+
 text = str()
-with open("../dumps-text/ru_total2.txt", "r") as f:
+with open("./dumps-text/ru_total2.txt", "r") as f:
     text = f.read()
 
 # passage en minuscule et suppression des ponctuations et lignes vides
@@ -15,6 +17,12 @@ text = text.lower()
 spec_chars = string.punctuation + '\n\xa0«»\t—…'
 text = re.sub('\n', '', text)
 text = "".join([ch for ch in text if ch not in spec_chars])
+# suppression des mots contenant des caractères latins
+mots_texte = text.split()
+for idx, mot in enumerate(mots_texte):
+    if 'a' in mot or 'b' in mot or 'c' in mot or 'd' in mot or 'e' in mot or 'f' in mot or 'g' in mot or 'h' in mot or 'i' in mot or 'j' in mot or 'k' in mot or 'l' in mot or 'm' in mot or 'n' in mot or 'o' in mot or 'p' in mot or 'q' in mot or 'r' in mot or 's' in mot or 't' in mot or 'u' in mot or 'v' in mot or 'w' in mot or 'x' in mot or 'y' in mot or 'z' in mot :
+        mots_texte.remove(mot)
+text = " ".join(mots_texte)
 
 text_tokens = word_tokenize(text)
 text = nltk.Text(text_tokens)
@@ -28,7 +36,7 @@ text = nltk.Text(text_tokens)
 
 ## création du nuage de mots
 text_raw = " ".join(text)
-wordcloud = WordCloud(max_font_size=50, max_words=40, background_color="white").generate(text_raw)
+wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text_raw)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
